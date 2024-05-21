a02baa2842434bc9868a550f2aeb3586ff487817
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-05-22 01:53:27 +0300

    feature #4: added file system interface
    - added and exported function for reading custom `.schema` files
    - added and exported function for reading `.json` files and converting them into object
    
    closes #4

---
+++ interfaces/reader.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+function checkAccess(filePath: string) {
+  try {
+    fs.accessSync(filePath);
+  } catch (err) {
+    throw new Error(`No access to "${filePath}"`);
+  }
+}
+
+function checkExtension(filePath: string, requiredExt: string) {
+  const fileExt = path.extname(filePath);
+  if (requiredExt !== fileExt) {
+    throw new Error(
+      `The file "${filePath}" has wrong extension. Only ${requiredExt} allowed`
+    );
+  }
+}
+
+function validateFile(filePath: string, requiredExt: string) {
+  checkAccess(filePath);
+  checkExtension(filePath, requiredExt);
+}
+
+export function readSchema(filePath: string) {
+  const resolvedPath = path.resolve(filePath);
+  validateFile(resolvedPath, '.schema');
+
+  try {
+    return fs.readFileSync(resolvedPath, 'utf-8');
+  } catch (error) {
+    throw new Error(`Error reading or parsing file: ${error}`);
+  }
+}
+
+export function readService(filePath: string) {
+  const resolvedPath = path.resolve(filePath);
+  validateFile(filePath, '.json');
+
+  try {
+    const serviceData = fs.readFileSync(resolvedPath, 'utf8');
+    return JSON.parse(serviceData);
+  } catch (error) {
+    throw new Error(`Error reading or parsing file: ${error}`);
+  }
+}
